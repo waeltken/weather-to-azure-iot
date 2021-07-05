b5c60e5aea98bd0bef91a06f9367d7066c8c73f0
Clemens Wältken <clemens.waltken@microsoft.com>
2021-07-05 08:05:45 +0100

    Add temperature correction using CPU temp

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from sense_hat import SenseHat
+from gpiozero import CPUTemperature
+
+FACTOR = 1
 
 sense = SenseHat()
+cpu = CPUTemperature()
+
 try:
 	while True:
-		temperature = sense.get_temperature_from_humidity()
+		cpu_temp = cpu.temperature
+		temp = sense.get_temperature_from_humidity()
+		temp_calibrated = temp - ((cpu_temp - temp)/FACTOR)
 		humidity = sense.get_humidity()
-		message = f"{temperature:.0f} C \n{humidity:.0f} %"
+		message = f"{temp_calibrated:.0f} C \n{humidity:.0f} %"
 		sense.show_message(message)
 except KeyboardInterrupt:
 	sense.clear()
